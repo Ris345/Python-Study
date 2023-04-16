962348dc56b9aac5ee6b5d33ec7ebfe805352a62
Dorjee <rishav345@gmail.com>
2023-04-16 11:12:50 -0400

    hw update

--- assign2.py
+++ assign2.py
@@ -16,6 +16,7 @@ class Students:
      #  show all the items
         return self.first, self.last, self.grade, self.batch, self.marks
 
+
 cl = Students('Rishav', 'Acharya', 2, 2018, [34, 67, 78])
 
 
@@ -24,83 +25,93 @@ cl = Students('Rishav', 'Acharya', 2, 2018, [34, 67, 78])
 
 # 2.
 
-# class Inventory: 
-#     def __init__(self):
-#         self.inventory ={}
-#     def add_items(self, item_id, item_name, stock_count, price):
-#         self.inventory[item_id] ={'item_name': item_name, 'stock_count': stock_count,'price': price}   
-#     def update_items(self,item_id, stock_count, price):
-#         # update items in inventory
-#         # return self.inventory.values()
-#         self.inventory[item_id] = item_id
-#         self.inventory['stock_count'] = stock_count
-#         self.inventory['price'] = price
-#     def check_item_details(self,item_id):
-#         # check item by id 
-#            if self.inventory[item_id] == item_id :
-#                return self.inventory.get(item_id)
+
+class Inventory:
+    def __init__(self):
+        self.inventory = {}
+
+    def add_items(self, item_id, item_name, stock_count, price):
+        # if item_id does not exist
+        if item_id not in self.inventory:
+            self.inventory[item_id] = {
+            'item_name': item_name, 'stock_count': stock_count, 'price': price}
+    def update_items(self, item_id, stock_count, price):
+        # update items in inventory by stock_price & price
+        if item_id in self.inventory:
+            self.inventory[item_id].update({
+                    'stock_count': stock_count,
+                    'price': price
+                })
+            
+    def check_item_details(self, item_id):
+        # check item by id
+        if item_id in self.inventory:
+            return self.inventory
+        else:
+            return 'provide correct ID'
 
 # il = Inventory()
-# print(il.add_items(1,'bag', 56, 70))   
-# print(il.update_items(2, 34,100)) 
+# print(il.add_items(2,'pants', 56, 70))
+# print(il.update_items(2, 34,100))
 # print(il.check_item_details(2))
-  
 
 
+# 3.
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
 
-# 3. 
-class Rectangle: 
-    def __init__(self,length,breadth):
-        self.length  = length
-        self.breadth = breadth 
-        
-        # return the self.item here 
+        # return the self.item here
     def perimeter(self):
-        # returns the permieter of the rectangle 
-            perimeter =  2 * (self.length + self.breadth)
-            return perimeter
+        # returns the permieter of the rectangle
+        perimeter = 2 * (self.length + self.breadth)
+        return perimeter
+
     def area(self):
-        # return the area of the rectangle 
-        area  = self.length * self.breadth 
+        # return the area of the rectangle
+        area = self.length * self.breadth
         return area
+
     def display(self):
         # return length, width, perimeter and the area of the rectangle
         print('length:',  self.length)
         print('breadth:', self.breadth)
         print('perimeter:', self.perimeter())
-        print ( 'area:', self.area())
+        print('area:', self.area())
 
-class Parallelipiped(Rectangle):    
-    def __init__(self,length,width,height):
-        super().__init__(length,width)  
-        self.height = height  
-        
+
+class Parallelipiped(Rectangle):
+    def __init__(self, length, width, height):
+        super().__init__(length, width)
+        self.height = height
+
     def volume(self):
-            # calculate the volume
-            volume =  self.length * self.breadth * self.height
-            print('volume:', volume)
+        # calculate the volume
+        volume = self.length * self.breadth * self.height
+        print('volume:', volume)
+
+# rc = Rectangle(2,2)
+# rc.perimeter()
+# rc.area()
+# rc.display()
+
+# ch = Parallelipiped(2,2,4)
+# ch.volume()
+
+
+# 4.
+class Book:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+        
+ 
+    def view(self):
+        return self.title, self.author, self.price
             
-rc = Rectangle(2,2)
-rc.perimeter()
-rc.area()
-rc.display()
-
-
-ch = Parallelipiped(2,2,4)
-ch.volume()
-
-
-# 4 
-# class Book:
-#      def __init__(self,title, author,price):
-#         self.title = title
-#         self.author = author
-#         self.price = price 
-#      def view(self):
-#         return self.title, self.author, self.price
-    
-# # bk = Book('To The LightHouse', 'Virginia Woolf','$23')
-# # print(list(bk.view()))
-# # print(isinstance(bk, Book))
-
+bk = Book('To The LightHouse', 'Virginia Woolf','$23')
+print(list(bk.view()))
+print(isinstance(bk, Book))
 
